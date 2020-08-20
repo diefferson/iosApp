7040cd4327cf421c1bb615f70bdffd8100d0ec58
Diefferson Santos <diefferson.sts@gmail.com>
2020-08-19 21:55:27 -0300

    Initial Commit

---
+++ iOSApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOSApp
+//
+//  Created by Diefferson Santos on 19/08/20.
+//  Copyright © 2020 Diefferson Santos. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
